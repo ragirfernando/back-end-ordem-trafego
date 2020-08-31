59b4c362316112b29f1e2253e79b7ad38cd18c6f
Ragir Fernando Aparecido da Silva <ragirfernando@gmail.com>
2020-08-31 08:32:26 -0300

    Mensagem de retorno.

--- src/main/resources/db/migration/V01__criar_e_registrar_categorias.sql
+++
@@ -1,6 +0,0 @@
- 
-CREATE TABLE categoria (
-	codigo SERIAL,
-	nome character varying(50) NOT NULL,
-	PRIMARY KEY (codigo)
-);
--- src/main/resources/db/migration/V02__inserir_categorias.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO categoria (nome) values ('Lazer');
-INSERT INTO categoria (nome) values ('Alimentacao');
-INSERT INTO categoria (nome) values ('Farmacia');
-INSERT INTO categoria (nome) values ('Supermercado');
-INSERT INTO categoria (nome) values ('Outros');
